09edcb8688a8a0b6fe9d3d64b3ef0d200b1faafd
danilo_bispo <danilob@lacunasoftware.com>
2022-08-07 11:43:59 -0300

    added a few more parsing functions, parsing for methods complete

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+from operator import index
+from pprint import pprint
 from typing import List
 import utils
 
@@ -5,12 +7,18 @@ import utils
 const_method_name = "Method name:"
 const_precondition = "__method_precondition_"
 const_predicate_arg = "_argument_"
+const_task_effect = "__task_effect"
 
 
 class FileMethodData:
-    def __init__(self, name, order):
+    def __init__(self, name, order, effects, preconditions):
         self.method_name = name
         self.order = order
+        self.effects = effects
+        self.preconditions = preconditions
+    def __repr__(self) -> str:
+        return f'MethodData: ("{self.method_name}","{self.order}", "{self.effects}", "{self.preconditions}")'
+
 
 def parse_method_name(method_line):
     name = method_line.split(const_method_name)[1]
@@ -21,29 +29,74 @@ def parse_method_name(method_line):
 def parse_precondition(line_prec):
     # Let's use destructuring to make the code a little cleaner
     predicate_name, predicate_type = line_prec.split(const_predicate_arg)[0], line_prec.split(const_predicate_arg)[1]
-    
     predicate_name, predicate_value = predicate_name.split("_")[0], predicate_name.split("_")[1]
-    print("predicate name:", predicate_name)
-    print("predicate value:", predicate_value)
-    print("predicate type: ", predicate_type)
+    # debug
+    # print("precondition predicate name:", predicate_name)
+    # print("precondition predicate value:", predicate_value)
+    # print("precondition predicate type: ", predicate_type)
     return utils.Precondition(name=predicate_name, type=predicate_type, value=predicate_value)
+
+
+def parse_effect(line_prec):
+    # Let's use destructuring to make the code a little cleaner
+    predicate_name, predicate_value = line_prec.split("_")[0], line_prec.split("_")[1]
+    predicate_type = line_prec.split(const_predicate_arg)[1]
+    # debug
+    # print("effect predicate name:", predicate_name)
+    # print("effect predicate value:", predicate_value)
+    # print("effect predicate type: ", predicate_type)
+    return utils.Effect(name=predicate_name, type=predicate_type, value=predicate_value)
 
 
 
 def parse_method_ordering(method_name:str, ordering_line: str):
     # First we split the line by whitespaces, which is the separator used for some things
+    print("Method name:", method_name)
     words = ordering_line.split(" ")
+    prec_list = []
+    effect_list = []
+    task_method_list = []
     for i in range(len(words)):
+        # precondition region
+        # Skip this line because we have already parsed the precondition
+        if(words[i].find(const_predicate_arg) >= 0 or words[i] == "\n" ): 
+            print("Ignored line:", words[i])
+            continue
         # An ordering always starts with a precondition method, so we check if
-        # 1) It exists and if it does we parse the condition and the
+        # 1) It exists and 
+        # 2) if it does we parse the value, the type and its name
         if(words[i].startswith(const_precondition)):
+            i_copy = i
             # Debug 
             # print("Prec print:", words[i+1])
-            prec = parse_precondition(words[i+1])
-            # This may change later
-            prec.tied_to = method_name
-        print(words[i])
-    return words
+            while(words[i_copy+1].find(const_predicate_arg) >= 0):
+                prec = parse_precondition(words[i_copy+1])
+                prec.tied_to = method_name
+                prec_list.append(prec)
+                i_copy = i_copy + 1
+            # I still have to send that precondition somewhere
+        # end precondition region
+
+        # effect region
+        elif(words[i].startswith(const_task_effect)):
+            i_copy = i
+            while(words[i_copy+1].find(const_predicate_arg) >= 0):
+                effect = parse_effect(words[i_copy+1])
+                effect.tied_to = task_method_list[-1]
+                effect_list.append(effect)
+                i_copy = i_copy + 1
+            # I still have to send that precondition somewhere
+        # end effect region
+
+        # Now they're definitely tasks or methods. This also represents the order of the tasks
+        else:
+            task_method_list.append(words[i])
+    print("Task order: ", task_method_list)
+    for preco in prec_list:
+        print("Precondition:", preco.name, "\n\tType:", preco.type, "\n\tvalue: ", preco.value)
+    for effect in effect_list:
+        print("Effect:", effect.name, "\n\tType:", effect.type, "\n\tvalue: ", effect.value, "\n\tTied to: ", effect.tied_to)
+    return prec_list, effect_list, task_method_list
 
 
 
@@ -59,18 +112,17 @@ def open_file(filename) -> List[FileMethodData]:
                 break
             else:
                 method_name_parsed=parse_method_name(method_line=method_name_line)
-                parsed_data.append(
-                    FileMethodData(
-                        name=method_name_parsed, 
-                        order=parse_method_ordering(
+                preconditions, effects, order = parse_method_ordering(
                             method_name=method_name_parsed, 
-                            ordering_line=method_ordering_line)))
+                            ordering_line=method_ordering_line)
+                parsed_data.append(FileMethodData(name=method_name_parsed, 
+                preconditions= preconditions, effects= effects, order=order))
             if not method_ordering_line: 
                 break  # EOF/
-    return parsed_data
     # Debug
-    for line in data:
-        print(line)
+    for obj in parsed_data:
+        print(repr(obj))
+    return parsed_data
 
 method_ordering_data = open_file("method_orderings.txt")
     
--- utils.py
+++ utils.py
@@ -4,3 +4,22 @@ class Precondition:
         self.type = type
         self.value = value
         self.tied_to = tied_to if tied_to is not None else None
+    
+    def __repr__(self) -> str:
+        return f'Precondition: ("{self.name}","{self.type}", "{self.value}", "{self.tied_to}")'
+
+class Effect:
+    def __init__(self, name, type, value, tied_to=None):
+        self.name = name
+        self.type = type
+        self.value = value
+        self.tied_to = tied_to if tied_to is not None else None
+        
+    def __repr__(self) -> str:
+        return f'Effect:  ("{self.name}","{self.type}", "{self.value}", "{self.tied_to}")'
+
+class Location:
+    def __init__(self, name, preconditions = None, effects = None):
+        self.name = name
+        self.preconditions = preconditions if preconditions is not None else None
+        self.effects = effects if effects is not None else None
