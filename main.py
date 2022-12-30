cf67c85432da46d46c6cb05b1e7ed475bafe8f31
Danilo Bispo <danilobispogalvao@gmail.com>
2022-12-30 13:04:19 -0300

    regarding last commit: minor changes updated gitignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from uppaal_generator import execute_generator
-from parser import execute_parser
+from uppaal_parser import execute_parser
 
 def main():
     method_data, abstract_task_data, var_and_types_list, types_set, predicate_dict, goal_orderings, goal_properties_list = execute_parser()
