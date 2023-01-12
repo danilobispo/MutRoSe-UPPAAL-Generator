3da916d0868006a78f2b2b8ea89467d7cda66439
Danilo Bispo <danilobispogalvao@gmail.com>
2023-01-12 17:26:09 -0300

    Added measure metrics, however it has to be run separately

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from uppaal_generator import execute_generator
 from uppaal_parser import execute_parser
+# uncomment these to profile in cmd line with
+# python -m cProfile -o .\program.prof .\main.py
+# import cProfile
+# import re
+# cProfile.run('main()')
 
 def main():
     method_data, abstract_task_data, var_and_types_list, types_set, predicate_dict, goal_orderings, goal_properties_list = execute_parser()
